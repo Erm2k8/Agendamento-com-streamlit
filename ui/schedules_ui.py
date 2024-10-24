53c793834e8312ff837d1135f84982c8e0067614
Ermesson Andrade <ermesson2k8and@gmail.com>
2024-10-23 21:55:26 -0300

    gitignore updated

--- ui/schedules_ui.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-class Schedules:
-    @classmethod
-    def hello_world(cls):
-        st.title("Schedules")
--- ui/services_ui.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-class Services:
-    @classmethod
-    def hello_world(cls):
-        st.title("Services")
